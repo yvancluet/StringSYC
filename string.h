edd532f034912b29b2a888952e63a569063d0843
Salma Ghaouzy <ghaouzysalma@gmail.com>
2017-01-29 16:19:10 +0100

    Definition attributs et methodes

--- string.h
+++ string.h
@@ -1,1 +1,77 @@
+#include <cstddef>
+#include <iostream>
+
+class string{
+
+	public : //public methods
+
+// Constructors
+  
+  string ();
+  string(const string& str);
+  string (const char* s);
+	
+//Getters
+
+  inline int size (void) const; 
+  inline char* c_str (void) const;
+  inline int max_size(void) const;
+  inline int length(void) const;
+
+
+
+//Setters
+
+	
+
+//Operators
+
+	friend string operator+ (const string& A, char B);
+	string& operator= (const string& A);
+	string& operator= (char c);
+
+  
+//Public Methods
+
+  void clear (void);
+  void resize(int n,char c);
+  void resize(int n);
+
+
+  protected :  //Protected Methods
+
+//Attributes
+ 
+
+  int size_; // length of the String
+  char* tab; // tab of char (represents the string)
+  int capacity_; // tab size (room currently allocated)
+  static const int Sizemax = 100; //maximum possible tab room
+
+};
+
+
+//Getters' definitions
+
+
+inline int string::size () const {
+  return size_;
+}
+
+inline int string::length() const{
+  return size_;  
+}
+
+inline char* string::c_str () const {
+	return tab;
+}
+
+inline int string::max_size() const{
+  return Sizemax;
+}
+
+
+
+//Setters' definitions
+
 
